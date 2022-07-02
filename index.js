85413521e90f1a971dc06fcff5ff8ea310bf26ea
Lenin garizabalo <lcamargo@protonmail.com>
2022-07-02 12:51:45 -0500

    add db connection

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+const mongoose = require('mongoose');
+
 // const reminderJob = require('./src/jobs/reminder');
 const bot = require('./src/bot');
-const connectDb = require('./src/services/db/connection-db.service');
+const config = require('./src/config');
 const logger = require('./src/services/logger.service');
 
 async function startApp() {
   try {
-    await connectDb();
-    await bot.launch();
+    bot.launch();
+    await mongoose.connect(config.mongo_url);
+    logger.info('Database connected successfully');
     // reminderJob(bot).start();
-    logger.info('Starting application... ');
   } catch (err) {
     logger.error(err);
   }
