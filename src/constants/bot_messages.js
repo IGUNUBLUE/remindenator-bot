5dc365b4a10eb12d4ca0ebbf566543b0076d75fb
Lenin garizabalo <lcamargo@protonmail.com>
2022-07-02 12:25:39 -0500

    update messages

--- src/constants/bot_messages.js
+++ src/constants/bot_messages.js
@@ -3,4 +3,8 @@ module.exports = {
     executionReply: `👋🏻 Hi, I'm remindenator!\nI'm a bot created to help you remember important events. \nType /help to show you how to use my commands..`,
     log: 'Called /start',
   },
+  cmdAdd: {
+    executionReply: '',
+    log: 'Called /add',
+  },
 };
