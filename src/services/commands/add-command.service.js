7e96fd34bda97ee9eb5f31d8215897085721ce45
Lenin garizabalo <lcamargo@protonmail.com>
2022-07-03 01:21:26 -0500

    add command add

--- src/services/commands/add-command.service.js
+++ src/services/commands/add-command.service.js
@@ -1,25 +1,63 @@
-// -- "Add birthday" command: sends info and sets data to insert a birthday -- //
-const config = require('../../config');
-const botMessages = require('../../constants/bot-messages.constant');
-const db = require('../db/operations-db.service');
+const internalMessages = require('../../constants/internal-messages.constant');
+const formatDates = require('../../constants/format-dates.constant');
+const typeChats = require('../../constants/type-chats.constant');
+const regex = require('../../constants/regex.constant');
+const replaceCases = require('../../constants/replace-cases.constant');
 const logger = require('../logger.service');
+const EventModel = require('../../db/models/events.model');
+const getUuid = require('../../utils/get-uuid.utils');
+const importantEvents = require('../../constants/important-events.constant');
+const dateModule = require('../../utils/date-module.util');
 
-module.exports = async function add(ctx, state) {
-  logger.info(botMessages.cmdAdd.log);
-  const userId = ctx.message.from.id;
-  // Check if the birthday is already in the database
-  const result = await db.getDocuments(config.eventsCollection, [
-    {
-      $match: {
-        chat_id: ctx.message.chat.id,
-        user_id: userId,
-      },
-    },
-  ]);
+module.exports = async function addEvent(ctx) {
+  logger.info(internalMessages.commands.add.info.log);
+  const type = ctx.message.chat.type;
+  const textInCommand = ctx.message.text.replace('/add', '');
+  const eventName = textInCommand.split('|')[0].trim();
+  const eventDate = textInCommand.split('|')[1].trim();
 
-  if (result.length) {
-    return ctx.reply('el cumple existe');
-  } else {
-    return ctx.reply('el cumple no esta...');
+  if (type === typeChats.private || type === typeChats.channel) {
+    return ctx.replyWithMarkdown(internalMessages.commands.add.warn.typeChat);
   }
+  if (eventName.length <= 3) {
+    return ctx.reply(
+      internalMessages.commands.add.warn.minimumCharacterEventName
+    );
+  }
+  if (!regex.withOutSpecialCharacters.test(eventName)) {
+    return ctx.reply(internalMessages.commands.add.warn.validStringEventName);
+  }
+  if (!dateModule(eventDate, formatDates.user.events, true).isValid()) {
+    return ctx.reply(internalMessages.commands.add.warn.validDate);
+  }
+
+  const {
+    from: { id: userId, first_name: firstName, username },
+    chat: { id: chatId },
+  } = ctx.message;
+  const eventDescription = textInCommand.split('|')[2] || '';
+  const [day, month, year] = eventDate.split('-');
+  const isoDateFormat = `${year}-${month}-${day}`;
+  const newEvent = new EventModel({
+    _id: getUuid(),
+    chat_id: chatId,
+    user_id: userId,
+    username,
+    first_name: firstName,
+    event_date: isoDateFormat,
+    event_name: eventName,
+    event_description: eventDescription,
+  });
+
+  await newEvent.save();
+
+  if (importantEvents[eventName]) {
+    const message = internalMessages.commands.add.info.importantEvent.replace(
+      replaceCases.command.messages.importEventName,
+      importantEvents[eventName]
+    );
+    return ctx.replyWithMarkdown(message);
+  }
+
+  return ctx.replyWithMarkdown(internalMessages.commands.add.info.normalEvent);
 };
