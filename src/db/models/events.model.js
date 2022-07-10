a52925f60d3fd9dd2fcdf05e0176c25feca59856
Lenin garizabalo <lcamargo@protonmail.com>
2022-07-09 21:48:39 -0500

    add timestamps

--- src/db/models/events.model.js
+++ src/db/models/events.model.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 
-const eventsSchema = new mongoose.Schema({
-  _id: String,
-  chat_id: Number,
-  user_id: Number,
-  username: String,
-  first_name: String,
-  event_date: Date,
-  event_name: String,
-  event_description: String,
-});
+const eventsSchema = new mongoose.Schema(
+  {
+    _id: String,
+    chat_id: Number,
+    user_id: Number,
+    username: String,
+    first_name: String,
+    event_date: Date,
+    event_name: String,
+    event_description: String,
+  },
+  { timestamps: true }
+);
 
 const EventModel = mongoose.model('Events', eventsSchema);
 
